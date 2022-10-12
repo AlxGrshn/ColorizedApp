e9e56fc6cc323c8bdf4ef7445c4f04b4473f653d
Frysed <frysed@gmail.com>
2022-10-12 16:16:36 +0300

    bug fixing

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        changeColorView()
+        setValue()
     }
-
-
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        changeColorView()
+        switch sender {
+        case redSlider: redLabel.text = redSlider.value.roundFloat()
+        case greenSlider: greenLabel.text = blueSlider.value.roundFloat()
+        default: blueLabel.text = greenSlider.value.roundFloat()
+        }
+    }
+    
+    private func changeColorView() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue() {
+        redLabel.text = redSlider.value.roundFloat()
+        greenLabel.text = greenSlider.value.roundFloat()
+        blueLabel.text = blueSlider.value.roundFloat()
+    }
 }
 
+extension Float {
+    func roundFloat() -> String {
+        String(format: "%.2f", self)
+    }
+}
